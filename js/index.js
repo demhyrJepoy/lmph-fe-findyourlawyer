e613ee6c3b6495c7cdb6bcea7b5445af2ba5a2d2
Jeffrey John Javison <jeffreyjohn.javison@legalmatch.com>
2023-12-19 10:52:01 +0800

    added Other Categories

--- js/index.js
+++ js/index.js
@@ -26,9 +26,14 @@ const app = () => {
         return {zipCodeCityControl: zipCodeCityValue, categoryControl: categoryValue};
     }
 
+    const otherCategoriesContent = () => {
+        return '<h4>OTHER CATEGORIES</h4><small>Choose the categories that best fit your case</small><div class="mt-4 row row-cols-2"><div class=col><a href=#>Sed ut perspiciatis</a></div><div class=col><a href=#>Unde omnis iste</a></div><div class=col><a href=#>Natus error sit</a></div><div class=col><a href=#>Voluptatem accusantium</a></div><div class=col><a href=#>Doloremque laudantium</a></div><div class=col><a href=#>Totam rem aperiam</a></div><div class=col><a href=#>Eaque ipsa quae</a></div><div class=col><a href=#>Ab illo inventore</a></div><div class=col><a href=#>Veritatis et quasi</a></div><div class=col><a href=#>Architecto beatae</a></div><div class=col><a href=#>Vitae dicta</a></div><div class=col><a href=#>Sunt explicabo</a></div><div class=col><a href=#>Nemo enim ipsam</a></div><div class=col><a href=#>Voluptatem quia</a></div><div class=col><a href=#>Voluptas sit</a></div><div class=col><a href=#>Aspernatur aut</a></div><div class=col><a href=#>Odit aut fugit</a></div><div class=col><a href=#>Sed quia consequuntur</a></div><div class=col><a href=#>Dolores eos qui</a></div><div class=col><a href=#>Ratione voluptatem</a></div><div class=col><a href=#>Sequi nesciunt</a></div><div class=col><a href=#>Neque porro quisquam est</a></div><div class=col><a href=#>Qui dolorem ipsum</a></div><div class=col><a href=#>Quia dolor sit amet</a></div><div class=col><a href=#>Consectetur</a></div><div class=col><a href=#>Adipisci velit</a></div><div class=col><a href=#>Sed quia non</a></div><div class=col><a href=#>Numquam eius</a></div></div>';
+    }
+
     return {
         loadEvent: load,
-        controlValues: getValues
+        controlValues: getValues,
+        otherCategoriesContent: otherCategoriesContent
     }
 }
 
@@ -52,7 +57,7 @@ const revEngrModal = () => {
         if(options.content !== null && options.content !== undefined) {
             modalBody.innerHTML = options.content;
         } else {
-            console.error("Tsk tsk tsk. Put a content at least!");
+            console.error("Put a content at least!");
             return undefined;
         }
 
@@ -60,10 +65,9 @@ const revEngrModal = () => {
         //change modal title
         if(options.title !== null && options.title !== undefined) {
             modalTitle.innerHTML = options.title;
+            modalHeader.classList.remove('d-none');
         } else
-            modalHeader.remove();
-
-
+            modalHeader.classList.add('d-none');
 
         //attach close event to modal close triggers
         modalClosuresX.addEventListener("click", doCloseNow);
@@ -90,6 +94,7 @@ const revEngrModal = () => {
 
         modalProper.classList.add('show');
         modalProper.classList.add('d-block');
+        modalProper.classList.remove('d-none');
     }
 
     const doCloseNow = () => {
